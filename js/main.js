b434f3326a03034a4e5100df5210cd817d199e62
Oussama Seqqam <ouseqqam@e2r5p1.1337.ma>
2021-05-29 20:35:44 +0100

    operators

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
+var tab = [];
 function put (id) {
-    var b = document.getElementById(id);
-    b = Number(b);
-    console.log(typeof b);
-    document.getElementById('par').innerHTML = document.getElementById('par').innerHTML + b;
+    b = document.getElementById(id);
+    if (b.innerHTML != '=')
+        tab.push(b.innerHTML);
+    if (b.innerHTML == '=')
+        document.getElementById('par').innerHTML = eval(tab.join(' '));
+    else
+        document.getElementById('par').innerHTML = document.getElementById('par').innerHTML + b.innerHTML;
 }
